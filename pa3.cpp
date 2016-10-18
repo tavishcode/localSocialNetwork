488df272209430097e8d4e796f8d0ca580270466
tavishcode <tavish.gobindram@gmail.com>
2016-10-18 22:44:35 +0800

    update pa3

--- pa3.cpp
+++ pa3.cpp
@@ -1,32 +1,15 @@
-/*
- * pa3.cpp
- *
- *  Complete on this file and submit it the CASS before due day.
- *  Good luck. If you need to seek clarification of help regarding this assignment,
- *  please approach Kevin Wang via (kevinw@cse.ust.hk) or write a post on the course facebook group.
- */
-
 #include <iostream>
 #include <cstring>
 #include "structure.h"
 
 using namespace std;
 
-
-/**
- * TODO: This function counts how many follow-up post in a post thread 'p'.
- */
 int count_followup(const Post* p)
 {
 	if(p->followup==NULL)
 		return 0;
 	return 1+count_followup(p->followup);
 }
-
-/**
- * TODO:  This function creates a Person structure in the memory and assigns the structure's
- * name with the given parameter. The initiate numbers of friends should be 0.
- */
 
 Person* create_user(const char* name)
 {
@@ -39,10 +22,6 @@ person->friends[0] = NULL;
 return person;
 }
 
-/**
- * TODO:  This function searches a person from a list with the name matched with a given
- *  string. It returns the address of the Person if it is found, or returns NULL if no such person is found.
- */
 Person* search_user_by_name(Person** list, int list_size,  const char* name)
 {
 for(int i=0;i<list_size;i++)
@@ -55,15 +34,6 @@ for(int i=0;i<list_size;i++)
 return NULL;
 }
 
-/**
- * TODO: Given a list (array) of Person pointers 'list' with the size 'size', this function adds a
- * Person pointer 'new_person' into the list. However, if the 'person' is already in the
- * list, it will simply return without doing anything. In this function, you should take
- * care the size of the list and suitable enlarging 'list' using dynamic memory management
- * techniques.
- * In this function, you may always assume the size of list is correct. If the size is 0,
- * 'list' can be assumed empty without initialized.
- */
 void add_user_into_list(Person**& list, int size, Person* new_person)
 {
 if(size==0)
@@ -84,11 +54,6 @@ else if(search_user_by_name(list,size,new_person->name)==NULL)
 }
 }
 
-
-/**
- * TODO: This function will delete a thread of post from the memory. If there are any
- * follow up post, it should subsequently delete them as well
- */
 void delete_post(Post*& p)
 {
 	if(p==NULL)
@@ -100,14 +65,6 @@ void delete_post(Post*& p)
 	p=NULL;
 }
 
-/**
- * TODO: Given the post title 'title', post content 'content', the owner of the post 'person',
- * this function should create a new post and return the pointer of this post.
- *
- * In case this post is a follow-up post of another post, the parameter 'reply_to' should not
- *  be NULL. It should be the pointer of the post being followed-up. In this case the post
- *  'reply_to' should link to this new_post as well.
- */
 Post* new_post(const char* title, const char* content, Person* person, Post* reply_to)
 {
 	if(reply_to==NULL)
@@ -145,12 +102,6 @@ Post* new_post(const char* title, const char* content, Person* person, Post* rep
 	return NULL;
 }
 
-/**
- * TODO: This function checks whether Person 'a' and 'b' are friends (return true if they
- * are). Please note that friendship is commutative but not associative. That means, if A
- * is B's friend, B should be A's friend. But if A and B are C's friends, A and B may or
- * may not be friend.
- */
 bool is_friend(const Person* a, const Person* b)
 {
 	if(search_user_by_name(a->friends,a->num_of_friends,b->name)==NULL)
@@ -158,12 +109,6 @@ bool is_friend(const Person* a, const Person* b)
 	return true;
 }
 
-/**
- * TODO: This function make Person 'a' and 'b' friends. Under one of the following
- * conditions, this function will abort and return false:
- * 		1) 'a' and 'b' is actually the same person. (One cannot be its own friend); or
- * 		2) 'a' and 'b' are friend already.
- */
 bool add_friend(Person* a, Person* b)
 {
 	if(a==b || is_friend(a,b))
@@ -189,16 +134,7 @@ bool add_friend(Person* a, Person* b)
 	b->num_of_friends++;
 	return true;
 }
-/**
- * TODO: This function will print all posts of a user. This function must use our
- * given function void print_post(const Post*). The order of post
- * printing must follow the original order in the variable 'posts'. In other words,
- * if post A and B are to printed and A is placed before B on the list (array) 'posts', A
- * should print before B. In this function, it is very important to note that you must
- * not do any cout or printf.
- * In this function, you may always assume the size of 'posts' is correct. If the size
- * is 0, 'post' can be assumed empty without initialized.
- */
+
 void print_a_user_post(Post const* const* posts, int size, const Person* p)
 {
 	for(int i=0;i<size;i++)
@@ -208,14 +144,6 @@ void print_a_user_post(Post const* const* posts, int size, const Person* p)
 	}
 }
 
-/**
- * TODO: This function create a list of friends-of-friends of the Person 'person' and
- * save it in the list 'list' (dynamic array of pointers). It also returns the size of this list. The definition of
- * friends-of-friends of a person A is:
- * 		1) any person that is a friend of person A; or
- * 		2) any person that is a friend of a friend of person A, excluding person A himself.
- *
- */
 int get_friends_of_friends(const Person* person, Person**& list)
 {
 	int count=0;
